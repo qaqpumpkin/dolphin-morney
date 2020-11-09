66c7fa0340e57deaa74c11715399f3b9766c00c3
pumpkin <648102110@qq.com>
2020-11-10 00:33:27 +0800

    feat(countMoney)

---
+++ src/router/index.ts
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Home from '../views/Home.vue'
+import Money from '@/views/Money.vue';
+import Labels from '@/views/Labels.vue';
+import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
+
+Vue.use(VueRouter)
+
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    redirect: '/money'
+  },
+  {
+    path: '/money',
+    component: Money
+  },
+  {
+    path:'/labels',
+    component: Labels
+  },
+  {
+    path:'/Statistics',
+    component: Statistics
+  },
+  {
+    path:'*',
+    component: NotFound
+  }
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>当前页面不存在</div>
+        <router-link to="/">返回首页</router-link>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'NotFound'
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
