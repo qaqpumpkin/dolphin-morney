5239e4781b95ab7550add2c3f93af50bcbc1a4df
babazi <648102110@qq.com>
2020-11-11 22:06:08 +0800

    feat(icon)

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -13,15 +13,5 @@
 </script>
 
 <style lang="scss" scoped>
-  .nav-wrapper {
-    border:1px solid green;
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-  }
-  .content {
-    border: 1px solid blue;
-    overflow: auto;
-    flex-grow: 1;
-  }
+
 </style>
