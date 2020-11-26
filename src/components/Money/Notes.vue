e3c12466b5400df58c1045844f3b32874ed3706a
babazi <648102110@qq.com>
2020-11-26 21:29:34 +0800

    备注

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,27 +1,37 @@
 <template>
     <label class="notes">
         <span class="name">备注</span>
-        <input type="text" placeholder="在这里输入">
+        <input
+            type="text"
+            v-model="value"
+            placeholder="在这里输入">
     </label>
 </template>
 
-<script>
-export default {
-name: "Notes"
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Notes extends Vue {
+    value = '';
 }
+
 </script>
 
 <style lang="scss" scoped>
-.notes{
+.notes {
     font-size: 14px;
     background: #f5f5f5;
     padding-left: 16px;
     display: flex;
     align-items: center;
-    .name{
+
+    .name {
         padding-right: 16px;
     }
-    input{
+
+    input {
         height: 64px;
         flex-grow: 1;
         background: transparent;
